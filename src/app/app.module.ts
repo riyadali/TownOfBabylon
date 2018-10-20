0d30021e12c3d948ea7fa80223e28a4a8057d50a
riyadali <riyadali@gmail.com>
2018-10-20 15:55:49 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,8 @@ import { NavbarBrandComponent } from './navbar-brand/navbar-brand.component';
 import {MyCalendarComponent} from './calendar/calendar.component';
 import {MyCalendarMoviesComponent} from './calendar-movies/calendar-movies.component';
 
+import { ngIcal } from 'angular-ical';
+
 /* To include Matt Lewis calendar -- refer to site https://github.com/mattlewis92/angular-calendar */
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
@@ -125,7 +127,7 @@ import { PageDoingBusinessInBabylonComponent } from './page-doing-business-in-ba
     PageDoingBusinessInBabylonComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [TransactionService, SearchService]
+  providers: [TransactionService, SearchService ]
 })
 export class AppModule { }
 
