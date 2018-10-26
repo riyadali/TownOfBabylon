b8d3960ddca1bc0d563128a513621c1ed19c64bb
riyadali <riyadali@gmail.com>
2018-10-26 16:16:48 -0400

    Update typings.d.ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -6,8 +6,10 @@ interface NodeModule {
 
 declare module "ics-to-json"{ 
    /* export function icsToJson(p: string): any; */
-   export default function (string) : IIcsCalendarEvent;
+   export default function (string) : IcsParserPromise;
 }
+
+type IcsParserPromise = (x: string) => Promise<IIcsCalendarEvent[]>;
 
 interface IIcsCalendarEvent {
   startDate: string;
