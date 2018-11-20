bea5c35d3d3a9b32d6d04e05f8d0dd04a2e248a7
riyadali <riyadali@gmail.com>
2018-11-20 15:00:51 -0500

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-page-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
