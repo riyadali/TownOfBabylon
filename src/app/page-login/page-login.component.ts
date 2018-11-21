3e69fb31a75380ed8fe447cace7a68fe2e8f7350
riyadali <riyadali@gmail.com>
2018-11-21 12:52:53 -0500

    Update page-login.component.ts

--- src/app/page-login/page-login.component.ts
+++ src/app/page-login/page-login.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HeaderContent {  
+  title: string;
+  strapline: string;
+};
+
 @Component({
   selector: 'app-page-login',
   templateUrl: './page-login.component.html',
   styleUrls: ['./page-login.component.scss']
 })
 export class PageLoginComponent implements OnInit {
-
+  
+  loginHeader : HeaderContent = {
+    title: {"Sign in to "};
+    strapline: {"Town of Babylon"};
+  }
   constructor() { }
 
   ngOnInit() {
