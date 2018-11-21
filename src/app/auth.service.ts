4fd83088f60c8727753e54e5322b820f8218afe7
riyadali <riyadali@gmail.com>
2018-11-21 15:25:43 -0500

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,88 +1,74 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-/* To fix map is not a function from web site https://stackoverflow.com/questions/34515173/angular-http-get-with-typescript-error-http-get-map-is-not-a-function-in-n */ 
-/* import 'rxjs/add/operator/map'*/
-import {map, tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
 
   constructor(private http: HttpClient) {}
-  //?????????? lots of work to do her
+   
+  // angular university - https://blog.angular-university.io/angular-jwt-authentication/ - uses the following method
+  //  login(email:string, password:string ) {
+  //      return this.http.post<User>('/api/login', {email, password})
+  //          .do(res => this.setSession) 
+  //          .shareReplay();
+  //  }
+  //private setSession(authResult) {
+  //      const expiresAt = moment().add(authResult.expiresIn,'second');
+
+  //      localStorage.setItem('id_token', authResult.idToken);
+  //      localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+  //}    
   
-  
-   /* GET heroes whose name contains search term */
-  searchHeroes(term: string): Observable<Hero[]> {
-    if (!term.trim()) {
-      // if not search term, return empty hero array.
-      return of([]);
-    }
-    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
-      tap(_ => this.log(`found heroes matching "${term}"`)),
-      catchError(this.handleError<Hero[]>('searchHeroes', []))
-    );
+  saveToken function (token) {
+      localStorage.setItem('tob_id_token', token);
   }
   
-}
+  getToken function () {
+      return localStorage.getItem("tob_id_token");
+  }
+  
+  isLoggedIn function() {
+    var token = getToken();
 
-//migrate this functionality to angular 6
-function authentication ($http, $window) {
+    if(token){
+      var payload = JSON.parse(atob(token.split('.')[1]));
 
-    var saveToken = function (token) {
-      $window.localStorage['loc8r-token'] = token;
-    };
-
-    var getToken = function () {
-      return $window.localStorage['loc8r-token'];
-    };
-
-    var isLoggedIn = function() {
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+  
+  currentUser function() {
+    if(isLoggedIn()){
       var token = getToken();
-
-      if(token){
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-        return payload.exp > Date.now() / 1000;
-      } else {
-        return false;
-      }
-    };
-
-    var currentUser = function() {
-      if(isLoggedIn()){
-        var token = getToken();
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
-        return {
+      var payload = JSON.parse(atob(token.split('.')[1]));
+      return {
           email : payload.email,
           name : payload.name
-        };
-      }
-    };
-
-    register = function(user) {
-      return $http.post('/api/register', user).success(function(data){
-        saveToken(data.token);
-      });
-    };
-
-    login = function(user) {
-      return $http.post('/api/login', user).success(function(data) {
-        saveToken(data.token);
-      });
-    };
-
-    logout = function() {
-      $window.localStorage.removeItem('loc8r-token');
-    };
-
-    return {
-      currentUser : currentUser,
-      saveToken : saveToken,
-      getToken : getToken,
-      isLoggedIn : isLoggedIn,
-      register : register,
-      login : login,
-      logout : logout
-    };
+      };
+    }
   }
+  
+  register function(user) { 
+      // We are calling shareReplay to prevent the receiver of this Observable from accidentally 
+      // triggering multiple POST requests due to multiple subscriptions.
+      return this.http.post<User>('/api/register', user)
+        .do(res => this.saveToken(res.token))
+        .shareReplay();      
+  }
+            
+  login function(user) {
+      // We are calling shareReplay to prevent the receiver of this Observable from accidentally 
+      // triggering multiple POST requests due to multiple subscriptions.
+      return this.http.post<User>('/api/login', user)
+        .do(res => this.saveToken(res.token))
+        .shareReplay();  
+  }
+  
+  logout function() {
+      localStorage.removeItem("tob_id_token");
+  }
+         
+}
