2f6abe63eb762299b0a5bcc16a5cb90b2d762b72
riyadali <riyadali@gmail.com>
2018-11-21 13:42:33 -0500

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,3 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+/* To fix map is not a function from web site https://stackoverflow.com/questions/34515173/angular-http-get-with-typescript-error-http-get-map-is-not-a-function-in-n */ 
+/* import 'rxjs/add/operator/map'*/
+import {map, tap} from 'rxjs/operators';
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+  //?????????? lots of work to do her
+  
+  
+   /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+  
+}
+
+//migrate this functionality to angular 6
 function authentication ($http, $window) {
 
     var saveToken = function (token) {
