159df06f981454ab5722e99f99f7e275bf535387
riyadali <riyadali@gmail.com>
2018-11-29 13:29:14 -0500

    Update page-register.component.ts

--- src/app/page-register/page-register.component.ts
+++ src/app/page-register/page-register.component.ts
@@ -1,1 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
+
+interface HeaderContent {  
+  title: string;
+  strapline: string;
+};
+
+@Component({
+  selector: 'app-page-register',
+  templateUrl: './page-register.component.html',
+  styleUrls: ['./page-register.component.scss']
+})
+export class PageRegisterComponent implements OnInit {
+  
+  formError: string = "";
+  credentials = {
+    user: {
+            name : "",
+            email : "",
+            password : ""
+          } 
+  };
+  
+  // if you want to redirect to specific page after registering
+  // For example you may want to a particular calendar entry.
+  //returnPage = $location.search().page || '/'; 
+  returnPage='/'; // for now just redirect to home page
+  
+  registerHeader : HeaderContent = {
+    title: "Create a new Town of Babylon account",
+    strapline: ""
+  }
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+  
+  onSubmit () {
+      this.formError = "";
+      if (!this.credentials.user.name || !this.credentials.user.email || !this.credentials.user.password) {
+        this.formError = "All fields required, please try again";
+        return false;
+      } else {
+        this.doRegister();
+      }
+  }
+  
+  doRegister () {
+      this.formError = "";
+      this.authService
+        .register(this.credentials)
+        .subscribe({
+            next(x) { console.log('data: ', x); },
+            error(err) { this.formError = err.message;
+                          console.log('Some error '+err.message); 
+                       }
+        });
+    }
+
+}
 
