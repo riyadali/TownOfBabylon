af83c11297b198f76c5e00dba18fee8d86c361de
riyadali <riyadali@gmail.com>
2018-12-10 13:23:27 -0500

    Update page-login.component.ts

--- src/app/page-login/page-login.component.ts
+++ src/app/page-login/page-login.component.ts
@@ -34,9 +34,10 @@ export class PageLoginComponent implements OnInit {
   // if you want to redirect to specific page after registering
   // For example you may want to a particular calendar entry.  
   //returnPage = $location.search().page || '/'; 
+  //returnPage='/'; // for now just redirect to home page
   // The above is from the getting mean site (it may still be useful)
   // However, for now I use the redirectURL in authService to control redirection)
-  returnPage='/'; // for now just redirect to home page
+  
   
   loginHeader : HeaderContent = {
     title: "Sign in to Town of Babylon",
