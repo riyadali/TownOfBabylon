cac709b9dd530cd056698113bdad1b9b2eb37430
riyadali <riyadali@gmail.com>
2018-12-13 13:50:44 -0500

    Update auth.module.ts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,13 +6,15 @@ import { PageLoginComponent }    from '../page-login/page-login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 // page header component moved to common module because it
 // is being shared by app.module as well as auth.module
-import { AppCommonModule } from '../common.module';
+// *** discovered that header component was more trouble than its worth
+// *** so just updated pagelogin's html to remove need for header
+// import { AppCommonModule } from '../common.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    AppCommonModule,
+   // AppCommonModule,
     AuthRoutingModule
   ],
   declarations: [
