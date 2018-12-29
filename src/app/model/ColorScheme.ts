532e287f23b4caf722420d52e237869dc3a2617e
riyadali <riyadali@gmail.com>
2018-12-29 15:41:22 -0500

    Update ColorScheme.ts

--- src/app/model/ColorScheme.ts
+++ src/app/model/ColorScheme.ts
@@ -1,9 +1,9 @@
-export interface ColorScheme {
+export interface ColorScheme {  
+  id?: number;
+  name: string;
   /* may eventually want to explictly type these as "Color" --
      refer to this link for a start https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/color/color-tests.ts
   */
-  id: number;
-  name: string;
   primary: string;
   secondary: string;
 }
