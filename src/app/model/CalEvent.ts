f765c087d0996d157464c8c512f2f1b0395fad7a
riyadali <riyadali@gmail.com>
2018-12-29 14:21:32 -0500

    Create CalEvent.ts

---
+++ src/app/model/CalEvent.ts
@@ -0,0 +1,23 @@
+
+export class CalEvent {
+  id: number;
+  title: string;
+  description?: string;
+  start: Date;
+  allDay?: boolean;
+  end?: Date;      
+  color: ColorScheme;
+  //actions: string[];
+  resizable?: {
+        beforeStart: boolean;
+        afterEnd: boolean;
+  };
+  draggable?: boolean;
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
