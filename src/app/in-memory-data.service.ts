68c2e6c9ba7919aa2f36eda6a009fc9236e616a0
riyadali <riyadali@gmail.com>
2018-12-29 14:17:42 -0500

    Update in-memory-data.service.ts

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,23 +9,25 @@ import {
   format
 } from 'date-fns';
 
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
-
 
 export class InMemoryDataService implements InMemoryDbService {
+ 
+ // Some default color schemes
+  private colors = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3'
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF'
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA'
+    }
+  };
+  
  createDb() {
    const transactions = [
      { id: 11, name: 'Mr. Nice', blockHeight: 521795},
