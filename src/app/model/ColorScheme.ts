df4bc232058f23e7298f1b0132d1349545894ede
riyadali <riyadali@gmail.com>
2019-01-30 13:11:39 -0500

    Update ColorScheme.ts

--- src/app/model/ColorScheme.ts
+++ src/app/model/ColorScheme.ts
@@ -1,7 +1,8 @@
 export interface ColorScheme {  
   id?: number;
   // color schemes with no owners are the default color schemes
-  owner?: number;
+  // found out I did not need the owner field on client side
+  // owner?: number;
   name: string;
   /* may eventually want to explictly type these as "Color" --
      refer to this link for a start https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/color/color-tests.ts
