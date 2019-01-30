db603be2f939af57d54dedb418df30197681deaf
riyadali <riyadali@gmail.com>
2019-01-30 13:33:29 -0500

    Update ColorScheme.ts

--- src/app/model/ColorScheme.ts
+++ src/app/model/ColorScheme.ts
@@ -4,6 +4,7 @@ export interface ColorScheme {
   // found out I did not need the owner field on client side
   // owner?: number;
   name: string;
+  slug?: string;
   /* may eventually want to explictly type these as "Color" --
      refer to this link for a start https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/color/color-tests.ts
   */
