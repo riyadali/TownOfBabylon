ed76d4ddd419136fae2c5195eb56676211c2b120
riyadali <riyadali@gmail.com>
2019-02-02 16:41:49 -0500

    Update calendar-editable.component.ts

--- src/app/calendar-editable/calendar-editable.component.ts
+++ src/app/calendar-editable/calendar-editable.component.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable, Subject, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+// not sure about the import below for map ... but I needed to add it to get the map in processColorScheme to work
 import 'rxjs/add/operator/map';
 
 import { CalEvent } from '../model/CalEvent';
