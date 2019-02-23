032765a069bbdf3c2250dda3c72ab882bb84168e
riyadali <riyadali@gmail.com>
2019-02-23 13:24:15 -0500

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,5 @@
 const apiURL = "http://angular6-tour-heroes.herokuapp.com/api/";
-const apiSquarePaymentURL = " http://riystest.herokuapp.com/";
+// rewrite rule is setup to redirect to https://riystest.herokuapp.com/
+// primary purpose of rewrite rule is to allow CORS (changeOrigin is set to true)
+const apiSquarePaymentURL = "/paymentSquare"; 
 export { apiURL, apiSquarePaymentURL };
