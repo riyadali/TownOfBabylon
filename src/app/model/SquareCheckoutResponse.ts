cbe2793949fbc137e173e15033040ed960b30e4b
riyadali <riyadali@gmail.com>
2019-02-27 12:24:27 -0500

    Update SquareCheckoutResponse.ts

--- src/app/model/SquareCheckoutResponse.ts
+++ src/app/model/SquareCheckoutResponse.ts
@@ -1,5 +1,8 @@
 export interface SquareCheckoutResponse {
   id: string;
   checkout_page_url: string;
-  ask_for_shipping_address: boolean
+  ask_for_shipping_address?: boolean;
+  merchant_support_email?: string;
+  pre_populate_buyer_email? : string;
+  pre_populate_shipping_address? : Address;
 }
