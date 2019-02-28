05b3285b31b47b5de90739e6c754928d02424eb0
riyadali <riyadali@gmail.com>
2019-02-28 14:07:06 -0500

    Update SquareProcessPaymentRequest.ts

--- src/app/model/SquareProcessPaymentRequest.ts
+++ src/app/model/SquareProcessPaymentRequest.ts
@@ -1,4 +1,5 @@
-// Definition from https://docs.connect.squareup.com/api/connect/v2#type-
+// Loosely based on definition from https://docs.connect.squareup.com/api/connect/v2#endpoint-transactions-charge
+// Note this is a partial definition and the nonce field is really card_nonce
 export interface SquareProcessPaymentRequest {
   nonce: string;  
 }
