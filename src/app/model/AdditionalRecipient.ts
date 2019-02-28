eeeee3f7281ede694bfbe7cd7493ab99efe5d464
riyadali <riyadali@gmail.com>
2019-02-28 13:45:26 -0500

    Update AdditionalRecipient.ts

--- src/app/model/AdditionalRecipient.ts
+++ src/app/model/AdditionalRecipient.ts
@@ -1,5 +1,6 @@
 import { Money } from './Money';
 
+// Definition from https://docs.connect.squareup.com/api/connect/v2#type-
 export interface AdditionalRecipient {
   location_id : string;
   description : string;
