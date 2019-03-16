25e20d44bee2b48be80616712b13e492039e9572
riyadali <riyadali@gmail.com>
2019-03-16 13:19:00 -0400

    Update payment-square.component.ts

--- src/app/payment/payment-square/payment-square.component.ts
+++ src/app/payment/payment-square/payment-square.component.ts
@@ -23,6 +23,12 @@ export class PaymentSquareComponent implements OnInit, AfterViewInit {
 
   paymentForm; //this is our payment form object
   private testButtonClicked: boolean;
+  
+  private skuCheckbox = false;
+  private categoryCheckbox = false;
+  private locationsCheckbox = false;
+  private inStockCheckbox = false;
+  private priceCheckbox = false;
 
   ngOnInit() {
     let self=this;
